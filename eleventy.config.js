8354a646d6771543dba3c8c804bb9573eec9507d
zoepje <zoe.penner@hva.nl>
2024-10-21 21:03:25 +0200

    try components with shortcodes

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,18 @@
+import { Navigation } from "./src/_includes/components/Navigation.js";
+
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css/");
   eleventyConfig.addPassthroughCopy("src/fonts/");
   eleventyConfig.addPassthroughCopy("src/images/");
 
   eleventyConfig.addWatchTarget("src/css/");
-  
+
+  // Components:
+  eleventyConfig.addShortcode("link", function (url, svg, label) {
+    const isActive = this.page.url === url;
+    return `<a ${isActive ? 'class="active"' : ''} href="${url}">${svg} ${label}</a>`;
+  });
+  eleventyConfig.addShortcode("navigation", Navigation);
   return {
     dir: {
       input: 'src',
