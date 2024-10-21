134ff76c2bbc9b391246d50ea7057effd5676532
zoepje <zoe.penner@hva.nl>
2024-10-21 19:02:25 +0200

    opnieuw setup

---
+++ eleventy.config.js
@@ -0,0 +1,13 @@
+export default function (eleventyConfig) {
+  return {
+    dir: {
+      input: 'src',
+      includes: '_includes',
+      output: '_site',
+    },
+    templateFormats: ['md', 'njk', 'html'],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+  }
+};
