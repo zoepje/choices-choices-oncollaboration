0bcd906e59d875410342a481d3bcbc6e0acf7bdb
zoepje <zoe.penner@hva.nl>
2024-10-22 12:15:06 +0200

    try to make nav active class

--- src/_includes/components/navigation.js
+++ src/_includes/components/navigation.js
@@ -5,9 +5,7 @@ export function Navigation({}){
 <nav>
     <ul>
       <li>
-        <a href="/">
-          <svg class='icon' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/></svg>Home
-        </a>
+      <% link, url="/", path="<path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/>" label="Home"%>
       </li>
       <li>
         <a href="/webinars">
