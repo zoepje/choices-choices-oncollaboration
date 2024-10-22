2af8884075e92be3563b2439283fe8bc6746d818
zoepje <zoe.penner@hva.nl>
2024-10-22 13:09:23 +0200

    navigation component complete

--- eleventy.config.js
+++ eleventy.config.js
@@ -8,11 +8,6 @@ export default function (eleventyConfig) {
   eleventyConfig.addWatchTarget("src/css/");
 
   // Components:
-  eleventyConfig.addShortcode("link", function (url, path, label) {
-    const isActive = this.page.url === url;
-    return `<a ${isActive ? 'class="active"' : ''} href="${url}">
-    <svg class='icon' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">${path}</svg> ${label}</a>`;
-  });
   eleventyConfig.addShortcode("navigation", Navigation);
   return {
     dir: {
