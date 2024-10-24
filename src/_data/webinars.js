ae80f3f1f80688087039c050f272b051b33f63f4
zoepje <zoe.penner@hva.nl>
2024-10-24 14:48:45 +0200

    it should fetch data now??

--- src/_data/webinars.js
+++ src/_data/webinars.js
@@ -2,15 +2,14 @@ import EleventyFetch from "@11ty/eleventy-fetch"
 
 const baseURL = 'https://fdnd-agency.directus.app/items/'
 
-export async function getWebinars(){
-  const webinar = `${baseURL}avl_webinars`
-  const response = EleventyFetch(webinar, {
+export default async function (){
+  const webinar = `${baseURL}avl_webinars?fields=*,speakers.avl_speakers_id.*,categories.avl_categories_id.*&sort[]=-date&limit=4`
+  const response = await EleventyFetch(webinar, {
     duration: "1d",
     type: "json"
   });
 
   const webinars = response.data;
-  console.log(webinars);
   
   return webinars;
 }
