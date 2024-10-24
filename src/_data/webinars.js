19e176a97cdb5caf7d5c543951d1d254a1dd4087
zoepje <zoe.penner@hva.nl>
2024-10-24 16:15:20 +0200

    webinars css

--- src/_data/webinars.js
+++ src/_data/webinars.js
@@ -3,13 +3,13 @@ import EleventyFetch from "@11ty/eleventy-fetch"
 const baseURL = 'https://fdnd-agency.directus.app/items/'
 
 export default async function (){
-  const webinar = `${baseURL}avl_webinars?fields=*,speakers.avl_speakers_id.*,categories.avl_categories_id.*&sort[]=-date&limit=4`
+  const webinar = `${baseURL}avl_webinars?fields=*,speakers.avl_speakers_id.*,categories.avl_categories_id.*&sort[]=-date`
   const response = await EleventyFetch(webinar, {
     duration: "1d",
     type: "json"
   });
 
   const webinars = response.data;
-  
+
   return webinars;
 }
